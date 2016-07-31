1c18279932832414f65807be56165fb31c5880ee
mattlcbesc <mattl@cbesc.com>
2016-07-30 22:55:44 -0400

    Update boxstarter1.ps1

--- boxstarter1.ps1
+++ boxstarter1.ps1
@@ -1,1 +1,1 @@
-﻿start http://boxstarter.org/package/url?https://goo.gl/I4VoRi
+﻿start http://boxstarter.org/package/url?https://gist.githubusercontent.com/mattlcbesc/e7891bb1973f3bad2f939c6386bf4597/raw/2067d9c4475ccacaec1095d455ceb773b817b95f/setup1.txt
